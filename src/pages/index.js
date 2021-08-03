7e654feb3c28b9b3842c1cc7840eea1b7a2e3127
ArildStark <87885663+ArildStark@users.noreply.github.com>
2021-08-03 16:50:06 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,44 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import NavHeader from "../components/header"
+import Herosection from "../components/heroimg"
+import Aboutsection from "../components/aboutsection"
+import ProductSection from "../components/products-section"
+import Homefaqsection from "../components/homefaqsection"
+import Homecontactsection from "../components/homecontactsection"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+
+const home  = () =>  {
+
+return(
+
+<html>
+
+<NavHeader>
+</NavHeader>
+<Herosection>
+</Herosection>
+<Aboutsection></Aboutsection>
+<ProductSection></ProductSection>
+<Homefaqsection></Homefaqsection>
+<Homecontactsection></Homecontactsection>
+
+
+
+
+
+
+
+
+
+</html>
+
 )
 
-export default IndexPage
+}
+
+export default home
